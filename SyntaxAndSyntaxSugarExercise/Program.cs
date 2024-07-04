9f24ef9d0cf3b2d23360770dc25dd621ba4a6787
Dan Marino <technicaldanny302@gmail.com>
2024-07-04 05:31:44 -0700

    fixed error on line 20 & 21

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -6,16 +6,16 @@ namespace SyntaxAndSyntaxSugarExercise
     {
         static void Main(string[] args)
         {
-            int answer = 4;
-            string response;
-            if (answer < 9)
-            {
-                response = answer + " is less than nine";
-            }
-            else
-            {
-                response = answer + " is greater than or equal to nine";
-            }
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
 
             var answer = 4;
             var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
--- SyntaxAndSyntaxSugarExercise/obj/Release/net6.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
+++ SyntaxAndSyntaxSugarExercise/obj/Release/net6.0/SyntaxAndSyntaxSugarExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44cafacad74ce05fbe9c6997c716bed70b593155")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adbe4748a3400a34388e2ce93dd1b1f2993214c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SyntaxAndSyntaxSugarExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
